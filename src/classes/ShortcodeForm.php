2e3da6c57d448cc35d40bd3771240d1b48ab0ff1
Max Baun <max@d3applications.com>
2017-04-03 17:11:49 -0400

    adding label to form shortcode

--- src/classes/ShortcodeForm.php
+++ src/classes/ShortcodeForm.php
@@ -2,8 +2,6 @@
 
 namespace D3\SSN\Shortcodes;
 
-use D3\SSN\Recaptcha;
-
 class ShortcodeForm
 {
 	public static function init()
@@ -15,43 +13,14 @@ class ShortcodeForm
 	// D3LB Form Shortcode
 	public static function callback($args, $content = "")
 	{
+		$label = (isset($args['label'])) ? $args['label'] : 'Enter Social Security Number';
+
 		$ajax_url = admin_url('admin-ajax.php');
 
-		// setup our output variable
-		$output = '
-			<div class="ssn-validator">
-				<form id="ssn_validator_form" name="ssn_validator_form" class="ssn-validator-form" method="post"
-					action="/5-2/">
-					<div class="ssn-validator-form-inner">
-						<label>Enter Social Security Number</label>
-						<div class="ssn-validator-input-container inline-container inputs-container">
-							<input
-								type="text"
-								name="ssn_validator_first"
-								class="ssn-validator-input ssn-validator-input-first" maxlength="3"
-								/>
-							<span class="ssn-validator-input-separator">-</span>
-							<input
-								type="text"
-								name="ssn_validator_second"
-								class="ssn-validator-input ssn-validator-input-second"
-								maxlength="2"
-								/>
-							<small id="ssn_validator_form_response" class="ssn-validator-form-text"></small>
-						</div>
-						<div class="ssn-validator-input-container inline-container submit-container">
-							<input
-								class="ssn-validator-input"
-								type="submit"
-								name="ssn_validator_submit"
-								value="Search"
-								/>
-						</div>
-						'.Recaptcha::render().'
-					</div>
-				</form>
-			</div>
-		';
+		ob_start();
+		include_once(plugin_dir_path(dirname(__FILE__)) . 'templates/form.php');
+		$output = ob_get_clean();
+		ob_end_clean();
 
 		// output the results
 		return force_balance_tags($output);
---
+++ src/templates/form.php
@@ -0,0 +1,44 @@
+<?php
+
+use D3\SSN\Recaptcha;
+
+?>
+
+<div class="ssn-validator">
+	<form id="ssn_validator_form" name="ssn_validator_form" class="ssn-validator-form" method="post"
+		action="/5-2/">
+		<div class="ssn-validator-form-inner">
+			<label>
+				<?php
+					echo $label;
+				?>
+			</label>
+			<div class="ssn-validator-input-container inline-container inputs-container">
+				<input
+					type="text"
+					name="ssn_validator_first"
+					class="ssn-validator-input ssn-validator-input-first" maxlength="3"
+					/>
+				<span class="ssn-validator-input-separator">-</span>
+				<input
+					type="text"
+					name="ssn_validator_second"
+					class="ssn-validator-input ssn-validator-input-second"
+					maxlength="2"
+					/>
+				<small id="ssn_validator_form_response" class="ssn-validator-form-text"></small>
+			</div>
+			<div class="ssn-validator-input-container inline-container submit-container">
+				<input
+					class="ssn-validator-input"
+					type="submit"
+					name="ssn_validator_submit"
+					value="Search"
+					/>
+			</div>
+			<?php
+				echo Recaptcha::render()
+			?>
+		</div>
+	</form>
+</div>
