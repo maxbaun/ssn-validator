cee6e3995ee6b93262ad0e2503a53c2934a57a96
Max Baun <max@d3applications.com>
2017-04-04 16:31:01 -0400

    adding unit testing framework

---
+++ tests/SampleTest.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Sample_Plugin
+ */
+
+namespace D3\SSN;
+
+class SampleTest extends \WP_UnitTestCase
+{
+	/**
+	 * A single example test.
+	 */
+	public function testSample()
+	{
+		// Replace this with some actual testing code.
+		$this->assertTrue(true);
+	}
+
+	public function test2Sample()
+	{
+		$this->assertFalse(false);
+	}
+}
